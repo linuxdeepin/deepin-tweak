6b13d8ff1f40b621370347a4e713a5bf58e26ffd
Zhang Dingyuan <zhangdingyuan@deepin.org>
2022-12-09 15:43:40 +0800

    feat: add a new plugin
    support edit system environment by basic action.
    
    Log:

---
+++ plugins/org.deepin.tweak.environment/translations/en_US.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="16"/>
+        <source>System Environment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="17"/>
+        <source>change system environment for user</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="117"/>
+        <source>Please modify environment variables carefully.
+You must clearly understand the importance of system environment variables. This operation is irreversible.
+If there is a problem, please log in with tty and delete the $HOME/.dde_env file to restore the environment.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="126"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="145"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ plugins/org.deepin.tweak.environment/translations/zh_CN.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <source>Title height</source>
+        <translation type="vanished">标题栏高度</translation>
+    </message>
+    <message>
+        <source>Change window title height</source>
+        <translation type="vanished">修改标题栏高度</translation>
+    </message>
+    <message>
+        <source>Reset to default</source>
+        <translation type="vanished">重置为默认值</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation type="vanished">应用</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="16"/>
+        <source>System Environment</source>
+        <translation>系统环境变量</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="17"/>
+        <source>change system environment for user</source>
+        <translation>修改用户的系统环境变量</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="117"/>
+        <source>Please modify environment variables carefully.
+You must clearly understand the importance of system environment variables. This operation is irreversible.
+If there is a problem, please log in with tty and delete the $HOME/.dde_env file to restore the environment.</source>
+        <translation>请慎重修改环境变量，您必须明确知道系统环境变量的重要性。该操作不可逆，如果出现问题，请在 tty 登录，并删除 $HOME/.dde_env 文件来恢复环境。</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="126"/>
+        <source>Add</source>
+        <translation>添加</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="145"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+</context>
+</TS>
