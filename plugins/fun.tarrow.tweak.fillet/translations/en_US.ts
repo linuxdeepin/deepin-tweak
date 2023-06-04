84d8fcc3af3cb3a647aca5086b8c62a062c82d4f
student-ice <china_uos@163.com>
2023-06-04 17:52:28 +0800

    feat: add fillet size plugin
    添加圆角大小插件，可以更精细的设置圆角大小
    修复窗口菜单背景色异常

---
+++ plugins/fun.tarrow.tweak.fillet/translations/en_US.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="14"/>
+        <source>Fillet size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="15"/>
+        <source>Adjust window fillet size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="41"/>
+        <source>Fillet size (0-18)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="67"/>
+        <source>Reset to default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="86"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
