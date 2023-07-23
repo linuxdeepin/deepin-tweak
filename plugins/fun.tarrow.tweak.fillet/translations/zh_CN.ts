780e7905a540a76290e1a9c2406bd72fbb94c7c2
student-ice <tonimayloneya@gmail.com>
2023-07-23 19:10:45 +0800

    chore: add translation
    add translation

---
+++ plugins/fun.tarrow.tweak.fillet/translations/zh_CN.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="14"/>
+        <source>Fillet size</source>
+        <translation>圆角大小</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="15"/>
+        <source>Adjust window fillet size</source>
+        <translation>调整窗口圆角大小</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="41"/>
+        <source>Fillet size (0-18)</source>
+        <translation>圆角大小 (0-18)</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="67"/>
+        <source>Reset to default</source>
+        <translation>重置为默认值</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="86"/>
+        <source>Apply</source>
+        <translation>应用</translation>
+    </message>
+</context>
+</TS>
