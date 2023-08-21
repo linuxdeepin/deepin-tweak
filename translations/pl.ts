1ff13e63643e94f4e48bbbedf6f3f458a36ab4c7
Eryk Michalak <gnu.ewm@protonmail.com>
2023-08-21 11:13:44 +0200

    Added translation using Weblate (Polish)

---
+++ translations/pl.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name>CategoryModel</name>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/CategoryModel.qml" line="9"/>
+        <source>Welcome</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/CategoryModel.qml" line="15"/>
+        <source>Other</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Preload</name>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="32"/>
+        <location filename="../src/preloadplugin/Preload.qml" line="45"/>
+        <location filename="../src/preloadplugin/Preload.qml" line="51"/>
+        <source>Deepin Tweak</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="61"/>
+        <source>Special Thanks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="70"/>
+        <source>This project was developed with the great help of the deepin community. In the name of the entire community, we would like to thank all the developers and contributors who made this release possible.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="76"/>
+        <source>Interface Design</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="108"/>
+        <source>Program Development</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PreloadPlugin</name>
+    <message>
+        <location filename="../src/preloadplugin/preloadplugin.cpp" line="36"/>
+        <source>Deepin Tweak</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Welcome</name>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/Categories/Welcome.qml" line="21"/>
+        <source>Now,
+You can control.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/Categories/Welcome.qml" line="42"/>
+        <source>Welcome to the deepin advanced tool -- Deepin Tweak! 
+You can define deepin in an advanced way by selecting the desired items in the sidebar. From the desktop environment to the kernel, Deepin Tweak gives you a high degree of customizability. Now, let&apos;s explore! 
+If you have more suggestions for this software, please feel free to submit a Pull Request or Issue; if this software has helped you, you can consider donating to us, or why not also give us a Star for our project. your support is our motivation to continue development, we appreciate it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/Categories/Welcome.qml" line="90"/>
+        <source>Made by deepin Opensource Community with love.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
