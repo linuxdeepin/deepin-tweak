53406408aefd300c897689282bf658e1ba9e427d
Hosted Weblate <hosted@weblate.org>
2023-11-15 08:04:13 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (5 of 5 strings)
    
    Added translation using Weblate (Hungarian)
    
    Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Added translation using Weblate (Spanish)
    
    Translated using Weblate (Hungarian)
    
    Currently translated at 100.0% (11 of 11 strings)
    
    Added translation using Weblate (Hungarian)
    
    Translated using Weblate (Chinese (Simplified))
    
    Currently translated at 100.0% (11 of 11 strings)
    
    Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (11 of 11 strings)
    
    Translated using Weblate (Spanish)
    
    Currently translated at 45.4% (5 of 11 strings)
    
    Added translation using Weblate (Spanish)
    
    Co-authored-by: Béla Bajza <bajzabela@gmail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>
    Co-authored-by: yangyangdaji <1504305527@qq.com>
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/es/
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/hu/
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/org-deepin-tweak-environment/es/
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/org-deepin-tweak-environment/hu/
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/zh_Hans/
    Translation: deepin/System Environment Plugin (org.deepin.tweak.environment)
    Translation: deepin/Tweak (deepin-tweak)

---
+++ plugins/org.deepin.tweak.environment/translations/hu.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="16"/>
+        <source>System Environment</source>
+        <translation>Rendszerkörnyezet</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="17"/>
+        <source>change system environment for user</source>
+        <translation>Rendszerkörnyezet módosítása a felhasználó számára</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="117"/>
+        <source>Please modify environment variables carefully.You must clearly understand the importance of system environment variables. This operation is irreversible.If there is a problem, please log in with tty and delete the $HOME/.dde_env file to restore the environment. Please login again to apply changes.</source>
+        <translation>Kérjük óvatosan módosítsa a környezeti változókat. Világosan meg kell értenie a rendszerkörnyezeti változók fontosságát. Ez a művelet visszafordíthatatlan. Probléma esetén jelentkezzen be tty-vel, és törölje a $HOME/.dde_env fájlt a környezet visszaállításához. Kérjük jelentkezzen be újra a módosítások alkalmazásához.</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="126"/>
+        <source>Add</source>
+        <translation>Hozzáadás</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="145"/>
+        <source>Save</source>
+        <translation>Mentés</translation>
+    </message>
+</context>
+</TS>
