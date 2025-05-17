52425bb941fca2965e0fd8f5feb400219ff9c31a
Hosted Weblate <hosted@weblate.org>
2025-05-17 05:00:16 +0200

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (11 of 11 strings)
    
    Added translation using Weblate (Ukrainian)
    
    Translated using Weblate (Ukrainian)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Added translation using Weblate (Ukrainian)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Added translation using Weblate (Turkish)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 100.0% (11 of 11 strings)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 81.8% (9 of 11 strings)
    
    Added translation using Weblate (Turkish)
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Nuri KÜÇÜKLER <enatsek@gmail.com>
    Co-authored-by: Максим Горпиніч <maksimgorpinic4@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/main/tr/
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/main/uk/
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/org-deepin-tweak-environment/tr/
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/org-deepin-tweak-environment/uk/
    Translation: deepin/System Environment Plugin (org.deepin.tweak.environment)
    Translation: deepin/Tweak (main)

---
+++ translations/tr.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>CategoryModel</name>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/CategoryModel.qml" line="9"/>
+        <source>Welcome</source>
+        <translation>Hoşgeldiniz</translation>
+    </message>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/CategoryModel.qml" line="15"/>
+        <source>Other</source>
+        <translation>Diğer</translation>
+    </message>
+</context>
+<context>
+    <name>Preload</name>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="32"/>
+        <location filename="../src/preloadplugin/Preload.qml" line="45"/>
+        <location filename="../src/preloadplugin/Preload.qml" line="51"/>
+        <source>Deepin Tweak</source>
+        <translation>Deepin Tweak (İnce Ayar)</translation>
+    </message>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="61"/>
+        <source>Special Thanks</source>
+        <translation>Özel Teşekkürler</translation>
+    </message>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="70"/>
+        <source>This project was developed with the great help of the deepin community. In the name of the entire community, we would like to thank all the developers and contributors who made this release possible.</source>
+        <translation>Bu proje deepin topluluğunun önemli yardımlarıyla geliştirilmiştir. Bütün topluluk adına, bu sürümün gerçekleştirilmesini sağlayan geliştiricilere ve katkıda bulunan kişilere teşekkürü bir borç biliriz.</translation>
+    </message>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="76"/>
+        <source>Interface Design</source>
+        <translation>Arayüz Tasarımı</translation>
+    </message>
+    <message>
+        <location filename="../src/preloadplugin/Preload.qml" line="108"/>
+        <source>Program Development</source>
+        <translation>Program Geliştirme</translation>
+    </message>
+</context>
+<context>
+    <name>PreloadPlugin</name>
+    <message>
+        <location filename="../src/preloadplugin/preloadplugin.cpp" line="36"/>
+        <source>Deepin Tweak</source>
+        <translation>Deepin Tweak (İnce Ayar)</translation>
+    </message>
+</context>
+<context>
+    <name>Welcome</name>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/Categories/Welcome.qml" line="21"/>
+        <source>Now,
+You can control.</source>
+        <translation>Şimdi
+Kontrol sizde.</translation>
+    </message>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/Categories/Welcome.qml" line="42"/>
+        <source>Welcome to the deepin advanced tool -- Deepin Tweak! 
+You can define deepin in an advanced way by selecting the desired items in the sidebar. From the desktop environment to the kernel, Deepin Tweak gives you a high degree of customizability. Now, let&apos;s explore! 
+If you have more suggestions for this software, please feel free to submit a Pull Request or Issue; if this software has helped you, you can consider donating to us, or why not also give us a Star for our project. your support is our motivation to continue development, we appreciate it.</source>
+        <translation>Deepin gelişmiş aracı Deepin Tweak&apos;e hoşgeldiniz!
+Kenar çubuğundan istediğiniz öğeleri seçerek deepin&apos;i gelişmiş bir şekilde tanımlayabilirsiniz. Deepin Tweak size masaüstü ortamından çekirdeğe kadar yüksek düzeyde özelleştirme sağlar. Şimdi inceleyelim:
+Bu yazılımla ilgili önerileriniz varsa, Çekme İsteği (Pull Request) veya Yayın (Issue) talep edebilirsiniz. Bu yazılım size yardımcı oluyorsa, bağış yapmayı düşünebilir, ya da neden olmasın projemiz için bize Yıldız vermeyi düşünebilirsiniz. Desteğiniz bizim için çok önemlidir ve geliştirmeye devam etmek için bize motivasyon verir.</translation>
+    </message>
+    <message>
+        <location filename="../src/maincomponentplugin/qml/Categories/Welcome.qml" line="90"/>
+        <source>Made by deepin Opensource Community with love.</source>
+        <translation>Deepin Açık Kaynak Kodu Topluluğu tarafından sevgi ile yapılmıştır.</translation>
+    </message>
+</context>
+</TS>
