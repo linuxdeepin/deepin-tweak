26b6b7661d0fc5864ab181d5d417434a1cb27673
Hosted Weblate <hosted@weblate.org>
2026-07-23 12:01:37 +0200

    Translated using Weblate (Lao)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translated using Weblate (Lao)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Co-authored-by: BoneNI <bounkirdni@gmail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/main/lo/
    Translate-URL: https://hosted.weblate.org/projects/deepin/deepin-tweak/org-deepin-tweak-environment/lo/
    Translation: deepin/System Environment Plugin (org.deepin.tweak.environment)
    Translation: deepin/Tweak (main)

--- plugins/org.deepin.tweak.environment/translations/lo.ts
+++ plugins/org.deepin.tweak.environment/translations/lo.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../main.qml" line="16"/>
         <source>System Environment</source>
-        <translation type="unfinished"></translation>
+        <translation>ສະພາບແວດລ້ອມລະບົບ</translation>
     </message>
     <message>
         <location filename="../main.qml" line="17"/>
         <source>change system environment for user</source>
-        <translation type="unfinished"></translation>
+        <translation>ປ່ຽນແປງສະພາບແວດລ້ອມລະບົບສຳລັບຜູ້ໃຊ້</translation>
     </message>
     <message>
         <location filename="../main.qml" line="117"/>
         <source>Please modify environment variables carefully.You must clearly understand the importance of system environment variables. This operation is irreversible.If there is a problem, please log in with tty and delete the $HOME/.dde_env file to restore the environment. Please login again to apply changes.</source>
-        <translation type="unfinished"></translation>
+        <translation>ກະລຸນາແກ້ໄຂຕົວປ່ຽນສະພາບແວດລ້ອມຢ່າງລະມັດລະວັງ. ທ່ານຕ້ອງເຂົ້າໃຈຢ່າງຈະແຈ້ງກ່ຽວກັບຄວາມສຳຄັນຂອງຕົວປ່ຽນສະພາບແວດລ້ອມລະບົບ. ການດຳເນີນການນີ້ບໍ່ສາມາດກັບຄືນມາເປັນຄືເກົ່າໄດ້. ຖ້າມີບັນຫາ, ກະລຸນາເຂົ້າສູ່ລະບົບດ້ວຍ tty ແລະ ລົບໄຟລ໌ $HOME/.dde_env ເພື່ອກູ້ຄືນສະພາບແວດລ້ອມ. ກະລຸນາເຂົ້າສູ່ລະບົບອີກຄັ້ງເພື່ອໃຫ້ການປ່ຽນແປງມີຜົນ.</translation>
     </message>
     <message>
         <location filename="../main.qml" line="126"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>ເພີ່ມ</translation>
     </message>
     <message>
         <location filename="../main.qml" line="145"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>ບັນທຶກ</translation>
     </message>
 </context>
 </TS>
